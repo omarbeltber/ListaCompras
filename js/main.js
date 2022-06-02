014a18c0038cf49e9aedaa9e280f73eb7bfbc360
omarbeltber <omarbeltber@gmail.com>
2022-06-02 17:22:47 -0500

    Hicimos mejoras en el código para que al hacer refresh no se pierdan los datos

--- js/main.js
+++ js/main.js
@@ -87,12 +87,16 @@ agregar.addEventListener("click", (event) => {
     document.getElementById("alertValidaciones").style.display="none";
     contador++; 
     document.getElementById( "contadorProductos").innerHTML=contador;
+    localStorage.setItem("contadorProductos", contador);
     let precio = (Math.floor((Math.random() * 50)*100))/100;
     let cantidad = parseFloat(txtNumber.value);
     totalEnProductos += (cantidad<1)? Math.ceil(cantidad):parseInt(cantidad);
     document.getElementById("productosTotal").innerHTML= totalEnProductos;
+    localStorage.setItem("productosTotal", totalEnProductos);
      costoTotal+= (precio * cantidad);
      total.innerHTML = `$ ${costoTotal.toFixed(2)}`;
+     localStorage.setItem("precioTotal", costoTotal.toFixed(2));
+
     let tmp = `<tr>
     <th scope="row">${contador}</th>
     <td> ${txtNombre.value}</td>
@@ -119,6 +123,21 @@ txtNumber.addEventListener("blur", (event)=>{
 );
 
 
+window.addEventListener("load", function(){
+ if(localStorage.getItem("contadorProductos")!=null) {
+        contador = parseInt(localStorage.getItem("contadorProductos"));
+        document.getElementById("contadorProductos").innerHTML=contador;
+  }//if contadorProductos
+  if(localStorage.getItem("precioTotal")) {
+    costoTotal = parseInt(localStorage.getItem("precioTotal"));
+    document.getElementById("precioTotal").innerHTML=costoTotal;
+  }//if precioTotal
+    if(localStorage.getItem("productosTotal")) {
+        totalEnProductos = parseInt(localStorage.getItem("productosTotal"));
+        document.getElementById("productosTotal").innerHTML=totalEnProductos;
+  }//if productosTotal
+}
+);
 
 
 //agregar.onclick =
