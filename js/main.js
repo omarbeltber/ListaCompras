4cfcef0b7896a3c69f01fa164c9231d296b624ef
omarbeltber <omarbeltber@gmail.com>
2022-06-03 12:34:05 -0500

    localStorage

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 let contador =0;
 let costoTotal = 0;
 let totalEnProductos =0;
+ //Arreglo global para almacenar la lista de compras
+let datos = [];
+
 let element = document.getElementById("totalPrecio");
 element.innerHTML="Total en precio";
 
@@ -97,6 +100,15 @@ agregar.addEventListener("click", (event) => {
      total.innerHTML = `$ ${costoTotal.toFixed(2)}`;
      localStorage.setItem("precioTotal", costoTotal.toFixed(2));
 
+      //JSON
+      let elemento =`{"id":${contador}, "nombre":"${txtNombre.value}", "cantidad":${txtNumber.value}, "precio":${precio}}`;
+
+      datos.push(JSON.parse(elemento));
+
+      localStorage.setItem("elementosTabla", JSON.stringify(datos) );
+
+      console.log(datos);
+
     let tmp = `<tr>
     <th scope="row">${contador}</th>
     <td> ${txtNombre.value}</td>
@@ -136,6 +148,19 @@ window.addEventListener("load", function(){
         totalEnProductos = parseInt(localStorage.getItem("productosTotal"));
         document.getElementById("productosTotal").innerHTML=totalEnProductos;
   }//if productosTotal
+
+   if(this.localStorage.getItem("elementosTabla")!=null){
+     datos= JSON.parse(localStorage.getItem("elementosTabla"));
+      datos.foreach(element =>{
+        cuerpoTabla[0].innerHTML +=  `<tr>
+        <th scope="row">${element.id}</th>
+        <td> ${element.nombre}</td>
+        <td>${element.cantidad}</td>
+        <td>$ ${element.precio}</td>
+        </tr> `;
+      });
+   }// if elementosTabla
+
 }
 );
 
